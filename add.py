db44653ce2a333a142c8fe53acb2089bc4530b48
a.alekseev <a.alekseev@itr24.com>
2023-05-09 03:40:29 +0300

    some refactor

--- add.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('main.db')
-c = conn.cursor()
-
-c.execute('INSERT INTO amm_over(P,T,v,h,s,cp,k,a) VALUES (?,?,?,?,?,?,?,?)', (280.0, 370.0, 0.01932, 865.0, 5.3, 4.94, 21.0, 1080.0))
-
-conn.commit()
